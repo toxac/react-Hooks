da69224129fb45d3ec1d5a1949f4d38192ccf5fa
Tox Ac <56756301+toxac@users.noreply.github.com>
2020-08-15 09:59:56 +0530

    joke setup

---
+++ src/Jokes.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+
+const Jokes = (props) => {
+  const jokeURL =
+    "https://sv443.net/jokeapi/v2/joke/Anyhttps://sv443.net/jokeapi/v2/joke/Any";
+  // Initial state hook to store joke
+  const [joke, setJoke] = React.useState({});
+  const [jokeCount, setJokeCount] = React.useState(0);
+  const [error, setError] = React.useState({});
+
+  React.useEffect(() => {
+    // Using effects to fetch new jokes when component is mounted
+    // Effects will also run everytime the count increase and hence will fetch a new joke
+    axios
+      .get(jokeURL)
+      .then((response) => {
+        // handle success, storing new joke to state
+        setJoke(response.data);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+        setError(error);
+      });
+  }, [setJokeCount]);
+
+  const handleClick = () => {
+    // Increase count for number of jokes viewed
+    setJokeCount((prevState) => prevState + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}> Get New Joke </button>
+    </div>
+  );
+};
+
+export default Jokes;
